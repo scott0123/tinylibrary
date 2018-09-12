44c9b4864e37a9279e5587a685ba9f22d60d4488
Scott <Scott@Scott-Liu.com>
2018-09-12 01:24:56 -0500

    The books now remember where you left off last time and scrolls to that location again

--- tinylibrary/TextViewController.m
+++ tinylibrary/TextViewController.m
@@ -24,13 +24,32 @@
         if (success) {
             // Display the content of the document, e.g.:
             self.contentView.text = self.document.contents;
+            // Scroll to where it was before
+            // load the data from user preferences if applicable
+            NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+            NSString* key = [NSString stringWithFormat:@"%@+%@", self.document.fileURL.absoluteString, @"scrollOffset"];
+            CGPoint scrollOffset = CGPointFromString([defaults objectForKey:key]);
+            // there is currently a bug where changing animated to NO makes it not scroll at all
+            [self.contentView setContentOffset:scrollOffset animated:YES];
         } else {
             // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
         }
     }];
 }
 
-- (IBAction)dismissDocumentViewController {
+- (IBAction)donePressed {
+    // Save the current scroll offset
+    // save it directly to user preferences
+    NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+    NSString* key = [NSString stringWithFormat:@"%@+%@", self.document.fileURL.absoluteString, @"scrollOffset"];
+    CGPoint scrollOffset = self.contentView.contentOffset;
+    [defaults setObject:NSStringFromCGPoint(scrollOffset) forKey:key];
+    [defaults synchronize];
+    // dismiss this view controller
+    [self dismissDocumentViewController];
+}
+
+- (void)dismissDocumentViewController {
     [self dismissViewControllerAnimated:YES completion:^ {
         [self.document closeWithCompletionHandler:nil];
     }];
